90d57e4937b64009690c4a2b8e0ca78083f5db30
AlanThisis <97637433+AlanThisis@users.noreply.github.com>
2022-02-02 18:47:47 -0800

    fix all tests

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,37 +11,48 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         int pastCloseParen = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            
             // System.out.println("Value of current index before loop: " + currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
 
-            if(pastCloseParen == closeParen || nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+            if (pastCloseParen == closeParen || nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1
+                    || closeParen == -1) {
                 break;
             }
             pastCloseParen = closeParen;
-            
-            // System.out.println("Index of next open bracket: " + nextOpenBracket);
-            // System.out.println("Index of next open bracket - 1: " + (nextOpenBracket - 1));
-            if(nextOpenBracket != 0) {
-                if(!markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")){
-                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+
+            if (openParen == nextCloseBracket + 1 && nextCloseBracket != nextOpenBracket + 1) {
+
+                if (!markdown.substring(openParen + 1, closeParen).contains("\n")) {
+                    if (nextOpenBracket != 0) {
+                        if (!markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!")) {
+                            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+                        }
+                        currentIndex = closeParen + 1;
+                    } else {
+                        toReturn.add(markdown.substring(openParen + 1, closeParen));
+                        currentIndex = closeParen + 1;
+                    }
+
+                } else {
+                    System.out.println(markdown.indexOf("\n", openParen));
+                    currentIndex = markdown.indexOf("\n", openParen) - 1;
+                    pastCloseParen = 0;
                 }
-                currentIndex = closeParen + 1;
-            } else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
             }
             // System.out.println("Value of current index after loop: " + currentIndex);
         }
         return toReturn;
     }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
